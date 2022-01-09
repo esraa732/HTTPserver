25202165bf85a5e1431a8db922fefb1c4f89a69e
Esraa Hesham <64265311+esraa732@users.noreply.github.com>
2022-01-09 17:48:12 +0200

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -8,9 +8,9 @@ namespace HTTPServer
     static class Configuration
     {
         public static string ServerHTTPVersion = "HTTP/1.1";
-        public static string ServerType = "FCISServer";
+        public static string ServerType = "Server";
         public static Dictionary<string, string> RedirectionRules=new Dictionary<string, string> { {"",""} };
-        public static string RootPath = "C:\\inetpub\\wwwroot\\fcis1";
+        public static string RootPath = "C:\\inetpub\\wwwroot\\1";
         public static string RedirectionDefaultPageName = "Redirect.html";
         public static string BadRequestDefaultPageName = "BadRequest.html";
         public static string NotFoundDefaultPageName = "NotFound.html";
